8b4440ed2b60c4f7d28e3a1a30c5d0696578cdf3
palos92 <joloed@gmail.com>
2017-06-26 15:29:56 +0200

    Update VistaPDF.java

--- app/src/main/java/com/example/usuario/webesdi/VistaPDF.java
+++ app/src/main/java/com/example/usuario/webesdi/VistaPDF.java
@@ -1,9 +1,12 @@
 package com.example.usuario.webesdi;
+import android.app.DownloadManager;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 
 import com.github.barteksc.pdfviewer.PDFView;
@@ -17,14 +20,17 @@ import java.net.URL;
 
 public class VistaPDF extends BaseActivity {
 
-    static PDFView pdfView;
+    PDFView pdfView;
     private String nom;
     private String url="http://www.esdi.es/content/pdf/fent-memoria-2010_2011ok.pdf";
+    private DownloadManager dm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_pdf);
+        dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
         setTitle("PDF");
         //Recogemos la variable
         Intent Mainact = getIntent();
@@ -41,23 +47,38 @@ public class VistaPDF extends BaseActivity {
         new urlPdf().execute(url);
         pdfView = (PDFView) findViewById(R.id.pdfView);
 
+        findViewById(R.id.descargar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(url)) {
+                    throw new IllegalArgumentException("url cannot be empty or null");
+                }
+
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+
+                if (isExternalStorageWritable()) {
+                    String uriString = v.getContext().getExternalFilesDir(null) + "";
+                    File file = new File(uriString, Uri.parse(url).getLastPathSegment());
+                    Uri destinationUri = Uri.fromFile(file);
+                    request.setDestinationUri(destinationUri);
+                    dm.enqueue(request);
+                }
+            }
+        });
+
         //Funcion para cargar desde un asset, para que funcione, tendriamos que crear una carpeta de assets y guardar dentro el pdf
         //pdfView.fromAsset("musica.pdf").load();
+
+
+    }
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
     }
 
-    public void Descargar(View v) {
-        Intent Mainact = getIntent();
-        nom = Mainact.getStringExtra("nombre");
-        if (nom.equals("impresora")) {
-            new urlPdf.descargarFitxer().execute("http://www.esdi.es/content/pdf/musica-y-nuevos-espacios-de-comunicacio--769-n--digital.pdf", "musica-y-nuevos-espacios-de-comunicacio--769-n--digital.pdf");
-        } else if (nom.equals("ps")) {
-            new urlPdf.descargarFitxer().execute("http://www.um.es/aulasenior/saavedrafajardo/apuntes/doc/Manual_photoshop.pdf", "Manual_photoshop.pdf");
-        } else if (nom.equals("gimp")) {
-            new urlPdf.descargarFitxer().execute("http://dis.um.es/~jfernand/0506/smig/gimp.pdf", "gimp.pdf");
-        }
-    }
-
-    static class urlPdf extends AsyncTask<String,Void,InputStream>{
+    class urlPdf extends AsyncTask<String,Void,InputStream>{
 
         @Override
         protected InputStream doInBackground(String... strings) {
@@ -75,29 +96,9 @@ public class VistaPDF extends BaseActivity {
             return is;
         }
 
-        private static class descargarFitxer extends AsyncTask<String, Void, Void>{
-
-            @Override
-            protected Void doInBackground(String... strings) {
-                String fileUrl = strings[0];
-                String fileName = strings[1];
-                File folder = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
-                File pdfFile = new File(folder, fileName);
-
-                try{
-                    pdfFile.createNewFile();
-                }catch (IOException e){
-                    e.printStackTrace();
-                }
-                Descarregarpdf.downloadFile(fileUrl, pdfFile);
-                return null;
-            }
-        }
         @Override
         protected void onPostExecute(InputStream is){
             pdfView.fromStream(is).load();
         }
-
-
     }
 }
